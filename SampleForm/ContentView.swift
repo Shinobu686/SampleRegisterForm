393df8dfae3ef4eb4cee6130df9632ec9d0808c8
Shinobu686 <choppu.business@gmail.com>
2021-02-15 22:05:21 +0900

    first commit

---
+++ SampleForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleForm
+//
+//  Created by 久富稜也 on 2021/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
